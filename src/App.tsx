ba84cc8f78356d3a52873ee6dc929bb3f726388c
Eldo Roshi <eldoroshi@users.noreply.github.com>
2023-01-23 15:32:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -251,7 +251,7 @@ function App() {
   }
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex flex-col">
       <Navbar
         setIsInfoModalOpen={setIsInfoModalOpen}
         setIsStatsModalOpen={setIsStatsModalOpen}
